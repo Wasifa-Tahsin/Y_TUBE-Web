a4c0ec7c031c428a6c628a5ee39c464dd266b43d
Afia <arminakterafia@gmail.com>
2024-10-05 17:40:26 +0600

    practice

--- js/api.js
+++ js/api.js
@@ -8,11 +8,34 @@ const loadbtnCatagory = ()=>{
 
 }
 //loaded the videos
-const loadVideos = () => {
-    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos`) 
+const loadVideos = (searchtext = "") => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchtext}`) 
     .then(responce => responce.json())
     .then(data => displayVidios(data.videos))
     .catch((error) => console.log(error));
+}
+
+
+// details btn
+const loadDetails=async(videoId)=>{
+    console.log(videoId)
+    const uri= `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+    const res=await fetch(uri)
+    const data=await res.json()
+   displayDetails(data.video)
+}
+
+const displayDetails=(video)=>{
+console.log(video)
+const detailsContainer=document.getElementById('modal-content')
+detailsContainer.innerHTML=`
+<img src=${video.thumbnail} />
+<P>${video.description}</P>
+`
+// way-1
+// document.getElementById('showModalData').click()
+// way-2
+document.getElementById('my_modal_5').showModal()
 }
 //remove btn classs list
 const removeClass = () => {
@@ -60,8 +83,8 @@ const displayVidios = (videos) =>{
     if (videos.length == 0) {
         videoSection.classList.remove('grid')
         videoSection.innerHTML = `
-            <div class= "my-32">
-                <img class = "w-44 mx-auto"  src = "./asset/Icon.png" />
+            <div class= "min-h-[300px] flex flex-col gap-5 justify-center items-center mt-10">
+                <img class = "w-44 mx-auto"  src = "Icon.png" />
                 <h2 class="pt-5 text-center text-2xl font-bold"> No Content Here in this Category </h2>
             </div>
         `
@@ -92,14 +115,19 @@ const displayVidios = (videos) =>{
                 <div class = "flex items-center pl-2">
                     <h2 class = "text-lg text-gray-700 ">${video.authors[0].profile_name}</h2>
                     ${video.authors[0].verified == true ? 
-                    `<img class = "h-5 w-5 ml-1" src ="./asset/verify.png" />`: ""}
+                    `<img class = "h-5 w-5 ml-1" src ="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />`: ""}
                 </div>
+                <P> <button onclick="loadDetails('${video.video_id }')" class="btn btn-sm btn-error">Details</button></P>
             </div>
         </div>
         `;
         videoSection.append(createVideoSContainer);
     })
 }
+// search
+document.getElementById('search-input').addEventListener('keyup',(e)=>{
+    loadVideos(e.target.value)
+})
 // fucntion calling area 
 loadbtnCatagory();
 loadVideos();
